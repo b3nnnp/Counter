3b2adb0c611325ca6705e4f91c7f56ba5e02e5d8
Ben Perez <>
2023-02-19 05:23:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+  count += 1
+  countEl.textContent = count
+}
+
+let saveEl = document.getElementById("save-el")
+
+function save() {
+  let countStr = count + " - "
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.textContent += countStr
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
+}
